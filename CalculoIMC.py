aa80895e26943442dae13952a6841c7ef70e7710
Henrique Junior <juninkgs@gmail.com>
2024-07-19 16:43:36 -0300

    EX01 - Cálculo do IMC

---
+++ CalculoIMC.py
@@ -0,0 +1,9 @@
+# CÁLCULO DO IMC
+nome = 'Henrique Junior'
+altura = 1.65
+peso = 55
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
