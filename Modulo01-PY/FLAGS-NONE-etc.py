03d1f0532320006ecaf99cfb47b3420f05404ee9
Henrique Junior <juninkgs@gmail.com>
2024-07-22 08:36:12 -0300

    Flags, None, Is e Is not

---
+++ Modulo01-PY/FLAGS-NONE-etc.py
@@ -0,0 +1,20 @@
+# EX04 - FLAGS, NONE, IS E IS NOT
+# Flag(Bandeira) - Marcar um local
+# None = Não valor
+# is e is not = é ou não é(tipo, valor, identidade)
+# id = Identidade
+
+condicao = False
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
