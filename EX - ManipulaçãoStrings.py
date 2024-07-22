16c4fe5c9e090294feb5e779cdac35d8d0afc8f0
Henrique Junior <juninkgs@gmail.com>
2024-07-22 08:26:15 -0300

    Calculo do IMC

--- EX - ManipulaçãoStrings.py
+++
@@ -1,7 +0,0 @@
-# EX01 -  MANIPULAÇÃO DE SRINGS
-nome = str(input("Digite seu nome: ")).strip()
-print(f"Olá {nome}, irei mostrar seu nome em diversas formas!")
-# MOSTRO AO USUÁRIO O SEU NOME DE VÁRIAS FORMAS
-print(f"Seu nome em maiúsculo é: {nome.upper()}")
-print(f"Seu nome em minúsculo é: {nome.lower()}")
-print(f"Seu nome tem {len(nome) - nome.count(' ')} letras")
--- Módulo01-PYTHON/CalculoIMC.py
+++
@@ -1,9 +0,0 @@
-# CÁLCULO DO IMC
-nome = 'Henrique Junior'
-altura = 1.65
-peso = 55
-imc = peso / altura ** 2
-
-print(nome, 'tem', altura, 'de altura,',)
-print('pesa', peso, 'quilos e seu imc é',)
-print(imc)
