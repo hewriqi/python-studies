79839f9655fe661c0e9f5a5f0929e0bf2e59510a
Henrique Junior <juninkgs@gmail.com>
2024-07-23 15:09:27 -0300

    Concatenando e estendendo

---
+++ Modulo01-PY/Concatenando e estendendo.py
@@ -0,0 +1,10 @@
+"""
+extend = estende a lista
+  +    = concatena listaS
+"""
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+# VAI DAR NONE, POIS EXECUTEI UM MÉTODO/FUNÇÃO/AÇÃO QUE NÃO RETORNA NADA (EXTEND)
+lista_a.extend(lista_b)
+print(lista_a)
