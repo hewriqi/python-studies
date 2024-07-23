aeeb10ab8fa347f1591daec3c5ebf6cae96dab01
Henrique Junior <juninkgs@gmail.com>
2024-07-23 15:50:02 -0300

    EXERCÍCIO - Exiba os indices da lista

---
+++ Modulo01-PY/EXERCICIO-ExibaIndiceLista.py
@@ -0,0 +1,9 @@
+"""
+Exercício
+Exiba os índices da lista
+"""
+
+lista = ['Henrique', 'Satoru', 'Gojo']
+indices = range(0, len(lista))
+for i in indices:
+    print(i, lista[i])
--- Modulo01-PY/FOR-IN com tipo list.py
+++ Modulo01-PY/FOR-IN com tipo list.py
@@ -1,6 +1,6 @@
 """
 for in com listas
 """
-lista = ['Henrique', 'Gojo', 'Isabella']
-for nome in lista:
+lista = ['Henrique', 'Gojo', 'Isabella']  # POSSO ACESSAR ITEM POR ITEM NA LISTA
+for nome in lista:  # PARA CADA NOME NA MINHA LISTA, FAÇA QUALQUER COISA QUE VOCÊ QUISER, NO CASO EU PRINTEI HAHAHA
     print(nome, type(nome))
