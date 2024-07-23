1ab264ea880023c88c1b890bc047531f63de4fdb
Henrique Junior <juninkgs@gmail.com>
2024-07-23 11:19:35 -0300

    Começando a trabalhar com listas em PYTHON

--- Modulo01-PY/Listas.py
+++ Modulo01-PY/Listas.py
@@ -11,9 +11,10 @@ string = 'ABCDE'  # 5 caracteres (len)
 # print(bool([]))  # falsy
 # print(lista, type(lista))
 
-#        0    1      2              3    4
-#       -5   -4     -3             -2   -1
+#         0     1         2          3    4   # POSIÇÕES DENTRO DA LISTA
+#        -5    -4        -3         -2   -1   # POSIÇÕES DENTRO DA LISTA
 lista = [123, True, 'Luiz Otávio',  1.2, []]
-lista[-3] = 'Maria'
-print(lista)
+lista[-3] = 'Maria'  # AQUI EU MUDEI O 'LUIZ OTÁVIO' PARA MARIA
+print(lista)  # PRINTEI A LISTA INTEIRA
+# PRINTEI O INDICE 2 DA LISTA (MARIA, PORQUE MUDEI DE LUIZ OTÁVIO) E O TIPO DESSE INDICE 2, QUE NO CASO É UMA STRING.
 print(lista[2], type(lista[2]))
