f5b65ae39b4c9e9c33c12747938161dc7b323de4
Henrique Junior <juninkgs@gmail.com>
2024-07-24 13:51:04 -0300

    EXERCICIO01 - Menu de Opções

--- Modulo01-PY/Exercícios - PYTHON/EX01 - PYTHON.py
+++ Modulo01-PY/Exercícios - PYTHON/EX01 - PYTHON.py
@@ -1,8 +1,11 @@
+# EXERCICIO - MENU DE OPÇÕES
+
 from time import sleep
 from math import trunc
-num1 = float(input("Digite um número: "))
-num2 = float(input("Digite um número: "))
+num1 = float(input("Digite um número: "))  # PEÇO O 1o NÚMERO AO USUÁRIO
+num2 = float(input("Digite um número: "))  # PEÇO O 2o NÚMERO AO USUÁRIO
 pergunta = 0
+# ENQUANTO A RESPOSTA DESSA PERGUNTA ABAIXO FOR DIFERENTE DE 5 (5 = SAIR DO PROGRAMA) EU VOU CONTINUAR MOSTRANDO AO USUARIO O MENU DE OPÇÕES
 while pergunta != 5:
     pergunta = str(input("""\nQual operação vc deseja fazer?
 [1] SOMA
@@ -13,42 +16,42 @@ while pergunta != 5:
 
 >>>>>>>>>     """)).strip().upper()
 
-    if pergunta == '1':
+    if pergunta == '1':  # SE A RESPOSTA FOR 1, FAÇO A SOMA
         soma = num1 + num2
         print('=='*10)
         print(f'\nO resultado da SOMA foi de: [ {trunc(soma)} ]')
         print('=='*10)
         continue
-    elif pergunta == '2':
+    elif pergunta == '2':  # SE A RESPOSTA FOR 2, FAÇO A MULTIPLICAÇÃO
         mult = num1 * num2
         print('=='*10)
-        print(f'\nO resultado da MULTIPLICAÇÃO foi de: [ {mult} ]')
+        print(f'\nO resultado da MULTIPLICAÇÃO foi: [ {mult} ]')
         print('=='*10)
         continue
-    elif pergunta == '3':
+    elif pergunta == '3':  # SE A RESPOSTA FOR 3, MOSTRO O MAIOR NÚMERO
         if num1 > num2:
             print('=='*10)
             print(f'O número {num1} é MAIOR!')
             print('=='*10)
             continue
-        elif num2 > num1:
+        elif num2 > num1:  # SE A RESPOSTA FOR 3, MOSTRO O MAIOR NÚMERO
             print('=='*10)
             print(f'O número {num2} é MAIOR')
             print('=='*10)
             continue
-        else:
+        else:  # SE NEM UM NEM OUTRO É MAIOR, ENTÃO OS DOIS SÃO IGUAIS
             print('=='*10)
             print(f'O número {num1} e o {num2} são IGUAIS!')
             print('=='*10)
             continue
-    elif pergunta == '4':
+    elif pergunta == '4':  # SE A RESPOSTA FOR 4, O USUÁRIO PODERÁ DIGITAR NOVOS NÚMEROS
         print('=='*10)
         print('\nCONTINUANDO O PROGRAMA...')
         sleep(1.9)
         num1 = float(input("Digite um número: "))
         num2 = float(input("Digite um número: "))
         continue
-    elif pergunta == '5':
+    elif pergunta == '5':  # SE A RESPOSTA FOR 5, O USUÁRIO VAI FINALIZAR O PROGRAMA
         print('=='*10)
         print('SAINDO...')
         sleep(2.15)
