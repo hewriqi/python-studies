6dc3be3df8c5e31732db5d800b64ae38bd3e58b3
Henrique Junior <juninkgs@gmail.com>
2024-07-25 11:15:01 -0300

    Enumerate

--- Modulo01-PY/Desempacotamento-Tupla.py
+++ Modulo01-PY/Desempacotamento-Tupla.py
@@ -2,7 +2,7 @@
 # TUPLAS É IGUAL UMA LISTA, SÓ QUE IMUTÁVEL
 # USA O '_' (UNDERLINE) PARA UMA VÁRIAVEL QUE VOCÊ NÃO IRÁ UTILIZAR NO CÓDIGO
 nomes = ['Henrique', 'Neymar', 'Junior']
-nomes = tuple(nomes)
-# nomes = list(nomes)
-print(nomes[-1])
-print(nomes)
+# nomes = tuple(nomes) --> TRANSFORMO A LISTA EM UMA TUPLA
+# nomes = list(nomes) --> TRANSFORMO A TUPLA EM UMA LISTA
+print(nomes[-1])  # --> ESTOU PRINTANDO O ÚLTIMO NOME
+print(nomes)  # --> PRINTANDO A LISTA/TUPLA INTEIRA
