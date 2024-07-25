aaa2a426d0072b30e13553c7879ede0a7b28701c
Henrique Junior <juninkgs@gmail.com>
2024-07-25 10:33:51 -0300

    Introdução ao desempacotamento e tuplas

---
+++ Modulo01-PY/Desempacotamento-Tupla.py
@@ -0,0 +1,8 @@
+# INTRODUÇÃO AO DESEMPACOTAMENTO E TUPLAS
+# TUPLAS É IGUAL UMA LISTA, SÓ QUE IMUTÁVEL
+# USA O '_' (UNDERLINE) PARA UMA VÁRIAVEL QUE VOCÊ NÃO IRÁ UTILIZAR NO CÓDIGO
+nomes = ['Henrique', 'Neymar', 'Junior']
+nomes = tuple(nomes)
+# nomes = list(nomes)
+print(nomes[-1])
+print(nomes)
