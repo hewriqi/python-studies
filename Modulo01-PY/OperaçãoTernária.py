7439c2dff7573ec0b98dd56efc27201df831d4b5
Henrique Junior <juninkgs@gmail.com>
2024-07-26 12:03:17 -0300

    Operação ternária

---
+++ Modulo01-PY/OperaçãoTernária.py
@@ -0,0 +1,13 @@
+"""
+Operação ternária (condicional de uma linha só)
+<valor> if <condicao> else <outro valor>
+"""
+# condicao = 10 == 10
+# variavel = 'Verdadeira' if condicao else 'FALSA'
+# print(variavel)
+# digito = 9
+# novo_digito = digito if digito <= 9 else 0
+# novo_digito = 0 if digito > 9 else digito
+# print(novo_digito)
+
+print('Valor' if True else 'Outro valor' if False else 'Fim')
