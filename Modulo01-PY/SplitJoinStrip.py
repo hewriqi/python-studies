3533188682701c4c1bcce1e2d5d067793665f53a
Henrique Junior <juninkgs@gmail.com>
2024-07-26 10:53:13 -0300

    Split, Join e Strip nas STRINGS em Python

---
+++ Modulo01-PY/SplitJoinStrip.py
@@ -0,0 +1,16 @@
+"""
+split e join com list e str
+split - divide uma string ( usa espaços em branco para quebrar a palavra )
+join - une uma string
+"""
+frase = '   Olha só que   , coisa interessante          '
+lista_frases_cruas = frase.split(',')
+
+lista_frases = []
+for i, frase in enumerate(lista_frases_cruas):
+    lista_frases.append(lista_frases_cruas[i].strip())
+
+# print(lista_frases_cruas)
+# print(lista_frases)
+frases_unidas = ', '.join(lista_frases)
+print(frases_unidas)
