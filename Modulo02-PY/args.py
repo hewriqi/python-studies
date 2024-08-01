1cc951c3f948c8ea9c9bd9d74d6d42acfab7ec61
Henrique Junior <juninkgs@gmail.com>
2024-08-01 15:13:57 -0300

    Args em PYTHON

--- Modulo02-PY/args.py
+++ Modulo02-PY/args.py
@@ -3,12 +3,13 @@ args - Argumentos não nomeados
 * - *args (empacotamento e desempacotamento)
 """
 # Lembre-te de desempacotamento
-# x, y, *resto = 1, 2, 3, 4
-# print(x, y, resto)
+x, y, *resto = 1, 2, 3, 4
+print(x, y, resto)
 
 
-# def soma(x, y):
-#     return x + y
+def soma(x, y):
+    return x + y
+
 
 def soma(*args):
     total = 0
