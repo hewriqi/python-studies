a9a5c48952671d60088128c38bb9161221e1d2bd
Henrique Junior <juninkgs@gmail.com>
2024-08-02 14:45:49 -0300

    Funções de primeira classe em PYTHON

---
+++ Modulo02-PY/FunçõesPrimeiraClasse.py
@@ -0,0 +1,15 @@
+"""
+Higher Order Functions
+Funções de primeira classe
+"""
+
+
+def saudacao(msg):
+    return msg
+
+
+def executa(funcao, msg):
+    return funcao(msg)
+
+
+v = executa(saudacao)
