91d66328859f30a6f4e763b5ae190c9b86e5da65
Henrique Junior <dev.henriquebarros@gmail.com>
2024-08-09 16:44:44 -0300

    DESAFIO15-PY.py

--- Modulo02-PY/EXERCICIOS-DESAFIOS/DESAFIO15-PY.py
+++ Modulo02-PY/EXERCICIOS-DESAFIOS/DESAFIO15-PY.py
@@ -1,5 +1,8 @@
+"""
+Crie um código que seja capaz de encontrar e exibir a maior e a menor palavra da lista (em número de caracteres).
+"""
 palavras = ["python", "asimov", "código", "web", "programação"]
 maior_palavra = max(palavras, key=len)
-menor_palavra = min(palavras, key=not)
+menor_palavra = min(palavras, key=len)
 print(maior_palavra)
 print(menor_palavra)
