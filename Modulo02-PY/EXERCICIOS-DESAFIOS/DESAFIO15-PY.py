ad2cff3cbc24fe4b439e00a15adda9875f531ad2
Henrique Junior <juninkgs@gmail.com>
2024-08-09 16:38:41 -0300

    EXERCICIO03

---
+++ Modulo02-PY/EXERCICIOS-DESAFIOS/DESAFIO15-PY.py
@@ -0,0 +1,5 @@
+palavras = ["python", "asimov", "código", "web", "programação"]
+maior_palavra = max(palavras, key=len)
+menor_palavra = min(palavras, key=not)
+print(maior_palavra)
+print(menor_palavra)
