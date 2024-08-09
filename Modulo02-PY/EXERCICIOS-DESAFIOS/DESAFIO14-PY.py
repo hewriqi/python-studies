43b13c39646f62667a59844f6840f4eeca9e2e45
Henrique Junior <dev.henriquebarros@gmail.com>
2024-08-09 16:45:05 -0300

    DESAFIO14-PY.py

--- Modulo02-PY/EXERCICIOS-DESAFIOS/DESAFIO14-PY.py
+++ Modulo02-PY/EXERCICIOS-DESAFIOS/DESAFIO14-PY.py
@@ -5,9 +5,7 @@ Escreva uma função que, dado o valor da conta de um restaurante, calcule e exi
 
 def gorjeta(valor_conta):
     calculo_gorjeta = (valor_conta * 10) / 100
-    print(f'A conta de R${valor_conta}, o valor da gorjeta para o garçom é de R${
-          calculo_gorjeta}')
+    print(f'A conta de R${valor_conta}, o valor da gorjeta para o garçom é de R${calculo_gorjeta}')
     return calculo_gorjeta
-
 
 gorjeta(777)
