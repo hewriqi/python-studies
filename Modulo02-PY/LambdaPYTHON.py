48c0a6202a261b6a8f3ae725e8fbed2a482f1f65
Henrique Junior <dev.henriquebarros@gmail.com>
2024-08-16 11:30:11 -0300

    Lambda em PYTHON

---
+++ Modulo02-PY/LambdaPYTHON.py
@@ -0,0 +1,37 @@
+# Introdução à função lambda (função anônima de uma linha)
+# A função lambda é uma função como qualquer
+# outra em Python. Porém, são funções anônimas
+# que contém apenas uma linha. Ou seja, tudo
+# deve ser contido dentro de uma única
+# expressão.
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'Miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
+
+
+# ---- lambda ----]
+precos = [100, 500, 10, 25]  # LISTA
+
+
+# ARGUMENTO | RETURN ||| LISTA ONDE VAI SER APLICADA A FUNÇÃO.
+impostos = list(map(lambda x: x*0.3, precos))
+print()
+print(impostos)
+print()
