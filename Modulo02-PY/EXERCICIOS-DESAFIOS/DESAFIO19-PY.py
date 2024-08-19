90c8723b0c252dfa58c28227a0ffeee9f7fb426d
Henrique Junior <dev.henriquebarros@gmail.com>
2024-08-19 16:58:32 -0300

    DESAFIO19 - List Comprehension

---
+++ Modulo02-PY/EXERCICIOS-DESAFIOS/DESAFIO19-PY.py
@@ -0,0 +1,10 @@
+# Dada uma lista de palavras, crie uma nova lista contendo o tamanho de cada palavra.
+lista_palavras = ['Henrique', 'Neymar', 'Família']
+
+lista_nova_de_palavras = [
+    len(palavras)
+    for palavras in lista_palavras
+]
+print(lista_nova_de_palavras)
+
+# --- OU / OR ---
