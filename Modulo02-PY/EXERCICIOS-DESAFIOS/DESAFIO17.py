62c1f45ebe66455c07cf049d65b889ffaafd62a0
Henrique Junior <dev.henriquebarros@gmail.com>
2024-08-19 16:26:16 -0300

    DESAFIO17 - List Comprehension

---
+++ Modulo02-PY/EXERCICIOS-DESAFIOS/DESAFIO17.py
@@ -0,0 +1,8 @@
+# Crie uma lista com os números pares de 1 a 10.
+lista1 = [numero * 2 for numero in range(1, 6)]
+print(lista1)
+
+# ---- OU / OR ----
+
+lista2 = [numero for numero in range(2, 11, 2)]
+print(lista2)
