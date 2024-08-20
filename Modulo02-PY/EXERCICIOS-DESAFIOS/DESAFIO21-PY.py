7af553e1c97113a5ceadd343f4e7c0b0ec206f9e
Henrique Junior <dev.henriquebarros@gmail.com>
2024-08-20 10:40:09 -0300

    DESAFIO21 - List Comprehension

---
+++ Modulo02-PY/EXERCICIOS-DESAFIOS/DESAFIO21-PY.py
@@ -0,0 +1,7 @@
+# Crie uma lista com as letras maiúsculas de uma string.
+
+texto = 'asDczxAdsawqrVewqEqwIasesasdaD'
+
+lista_maiuscula_texto = [letra for letra in texto if letra.isupper()]
+
+print(lista_maiuscula_texto)
